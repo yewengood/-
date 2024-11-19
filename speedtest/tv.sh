9a4ac798a239d26739885bfaea9112548c0c5611
yewengood <152235769+yewengood@users.noreply.github.com>
2024-11-19 18:01:43 +0800

    Update tv.sh

--- speedtest/tv.sh
+++ speedtest/tv.sh
@@ -1,8 +1,57 @@
-rm -rf ip/Beijing.onlygood.ip
-city="Beijing"
+#!/bin/bash
+# cd /root/iptv
+# read -p "确定要运行脚本吗？(y/n): " choice
+
+# 判断用户的选择，如果不是"y"则退出脚本
+# if [ "$choice" != "y" ]; then
+#     echo "脚本已取消."
+#     exit 0
+# fi
+
+time=$(date +%m%d%H%M)
+i=0
+
+if [ $# -eq 0 ]; then
+  echo "请选择城市："
+   echo "1. 四川电信（Sichuan）"
+   echo "2. 北京联通（Beijing）"
+  echo "0. 全部"
+  read -t 10 -p "输入选择或在10秒内无输入将默认选择全部: " city_choice
+
+  if [ -z "$city_choice" ]; then
+      echo "未检测到输入，自动选择全部选项..."
+      city_choice=0
+  fi
+
+else
+  city_choice=$1
+fi
+
+# 根据用户选择设置城市和相应的stream
+case $city_choice in
+
+   1)
+        city="Sichuan_333"
+        stream="udp/239.93.42.33:5140"
+        ;;
+    2)
+        city="Beijing_liantong_145"
+        stream="rtp/239.3.1.236:2000"
+        ;;
+
+    0)
+@@ -64,77 +60,70 @@
 # 使用城市名作为默认文件名，格式为 CityName.ip
 ipfile="ip/${city}.ip"
 only_good_ip="ip/${city}.onlygood.ip"
+rm -f $only_good_ip
+# 搜索最新 IP
+echo "===============从 fofa 检索 ip+端口================="
+curl -o test.html "$url_fofa"
+#echo $url_fofa
+echo "$ipfile"
+grep -E '^\s*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+$' test.html | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+' > "$ipfile"
+rm -f test.html
 # 遍历文件 A 中的每个 IP 地址
 while IFS= read -r ip; do
     # 尝试连接 IP 地址和端口号，并将输出保存到变量中
@@ -13,34 +62,34 @@ while IFS= read -r ip; do
     # 如果连接成功，且输出包含 "succeeded"，则将结果保存到输出文件中
     if [[ $output == *"succeeded"* ]]; then
         # 使用 awk 提取 IP 地址和端口号对应的字符串，并保存到输出文件中
-        echo "$output" | grep "succeeded" | awk -v ip="$ip" '{print ip}' >> "ip/${city}.onlygood.ip"
+        echo "$output" | grep "succeeded" | awk -v ip="$ip" '{print ip}' >> "$only_good_ip"
     fi
 done < "$ipfile"
+
 echo "===============检索完成================="
 
 # 检查文件是否存在
-if [ ! -f "ip/${city}.onlygood.ip" ]; then
-    echo "错误：文件 ip/${city}.onlygood.ip 不存在。"
+if [ ! -f "$only_good_ip" ]; then
+    echo "错误：文件 $only_good_ip 不存在。"
     exit 1
 fi
 
-
-lines=$(wc -l < "ip/${city}.onlygood.ip")
-echo "【ip/${city}.onlygood.ip】内 ip 共计 $lines 个"
+lines=$(wc -l < "$only_good_ip")
+echo "【$only_good_ip】内 ip 共计 $lines 个"
 
 i=0
 time=$(date +%Y%m%d%H%M%S) # 定义 time 变量
 while IFS= read -r line; do
     i=$((i + 1))
     ip="$line"
-    url="http://$ip/udp/239.3.1.129:8008"
+    url="http://$ip/$stream"
     echo "$url"
     curl "$url" --connect-timeout 3 --max-time 10 -o /dev/null >zubo.tmp 2>&1
     a=$(head -n 3 zubo.tmp | awk '{print $NF}' | tail -n 1)
 
     echo "第 $i/$lines 个：$ip $a"
     echo "$ip $a" >> "speedtest_${city}_$time.log"
-done < "ip/${city}.onlygood.ip"
+done < "$only_good_ip"
 
 rm -f zubo.tmp
 awk '/M|k/{print $2"  "$1}' "speedtest_${city}_$time.log" | sort -n -r >"result_fofa_${city}.txt"
@@ -57,9 +106,15 @@ sed "s/ipipip/$ip1/g" "$program" > tmp1.txt
 sed "s/ipipip/$ip2/g" "$program" > tmp2.txt
 sed "s/ipipip/$ip3/g" "$program" > tmp3.txt
 cat tmp1.txt tmp2.txt tmp3.txt > "txt/fofa_${city}.txt"
+
 rm -rf tmp1.txt tmp2.txt tmp3.txt
-rm -rf gddx_fofa.txt
+
+rm -rf zubo_fofa.txt
 #--------------------合并所有城市的txt文件为:   zubo_fofa.txt-----------------------------------------
-echo "📡  电信广电,#genre#" >>gddx_fofa.txt
-cat txt/fofa_Beijing.txt >>gddx_fofa.txt
+
+echo "📡  四川频道,#genre#" >>zubo_fofa.txt
+cat txt/fofa_Sichuan.txt >>zubo_fofa.txt
+echo "📡  北京联通,#genre#" >>zubo_fofa.txt
+cat txt/fofa_Beijing.txt >>zubo_fofa.txt
+
 for a in result/*.txt; do echo "";echo "========================= $(basename "$a") ==================================="; cat $a; done
